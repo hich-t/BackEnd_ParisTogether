f49b11a66da32c9ddb675fd46f24d9a445b8c260
Elyes MELE <mele.elyes@gmail.com>
2023-01-30 15:00:34 +0100

    BackEnd init

---
+++ Backend/Routes/authRouter.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const authRouter = express.Router();
+const User = require("../models/UsersModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+authRouter.use(express.json());
+
+authRouter.post("/registrer", async (req, res) => {
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(409).send("Email already exist");
+  if (req.body.password !== req.body.confirm_password)
+    return res.status(409).send("Confirmation password is not Ok");
+  if (!emailExist && req.body.password === req.body.confirm_password) {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: hashPassword,
+    });
+    user.save();
+
+    const token = jwt.sign({ user }, process.env.SECRET);
+    res.header("auth-token", token);
+    res.json(token);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email not found");
+  console.log(user)
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Password is not valid");
+
+  const token = jwt.sign({ user }, process.env.SECRET);
+  res.header({"auth-token": token});
+  res.json(token);
+});
+
+authRouter.get("/user", async (req,res)=> {
+let checkToken = jwt.verify(req.headers.authorization,process.env.SECRET) 
+let id = checkToken.user._id
+  User.findOne({_id : id})
+  .then(user => res.json(user))
+  .catch(err => res.json(err))
+})
+
+module.exports = authRouter;
